b1e390d9ec517dd5c2f80ba70ac8efdb26de00e0
rodnavarroc <rodolfo.0926@hotmail.com>
2020-07-09 18:42:37 -0500

    Creada interfaz de gastos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  product: any = {}; 
+  constructor(public http:HttpClient) {}
 
-  constructor() {}
-
+  insert(){
+    this.product.action = "insert"; 
+    this.http.post("http://localhost/economyhealth/gasto.php", this.product).subscribe(data=>{
+    console.log(data); 
+    let result = JSON.stringify(data["_body"]); 
+    }, err=>{
+    console.log(err); 
+    })
+    }
 }
