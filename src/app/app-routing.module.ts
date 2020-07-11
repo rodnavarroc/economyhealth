a25aa7928541181d8129d947a854db144605f01a
JoseFloresL <jose.flores.12@hotmail.com>
2020-07-11 13:54:43 -0500

    Crear diseño de página inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+  },
+  {
     path: 'start-page',
     loadChildren: () => import('./start-page/start-page.module').then( m => m.StartPagePageModule)
   }
