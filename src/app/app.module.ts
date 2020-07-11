b042552b06405fd7ae79ddcc0cc68771ed8ee2eb
Carlos <carlos7397@hotmail.com>
2020-07-11 13:55:02 -0500

    Componentes Menu y Header creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
