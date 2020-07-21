4591b5958f86cb31e7f41246501f8201b1f06dd3
Carlos <carlos7397@hotmail.com>
2020-07-21 13:05:02 -0500

    IngresosTest

--- src/app/ingresos/ingresos.page.ts
+++ src/app/ingresos/ingresos.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { HttpClient} from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Globals } from '../globals';
+
 @Component({
   selector: 'app-ingresos',
   templateUrl: './ingresos.page.html',
@@ -7,9 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresosPage implements OnInit {
 
-  constructor() { }
+  ingreso:any ={}
+  constructor(public http:HttpClient, public alertController: AlertController, public router : Router, public globals: Globals) { this.globals = globals;}
 
   ngOnInit() {
   }
 
+
+  insertDatosGasto(){
+    this.ingreso.action = "insert"; 
+    this.ingreso.idusuario = this.globals.username;
+    this.http.post("http://45.15.24.33/economyhealth_server/historial_ingresos.php", this.ingreso).subscribe(data=>{ 
+    console.log(data);
+    let result = JSON.stringify(data["_body"]);
+    if(data['status'] == "success")
+    {
+      this.successAlert();
+      this.router.navigate(['/tabs/tab1']);
+    }
+    }, err=>{
+    console.log(err); 
+    })
+  }
+
+  async successAlert() {
+    const alert = await this.alertController.create({
+      header: '¡Listo!',
+      message: 'Ingreso registrado correctamente.',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
